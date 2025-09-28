e90238c41b72c7b74a9088488d5a65ebadc6b643
Emmyfrosh001 <elijahchristopher343@gmail.com>
2025-09-29 00:07:34 +0100

    Update subzero.js

--- settings.js
+++ settings.js
@@ -1,7 +1,7 @@
 module.exports = {
-  SESSION_ID: "", // Put Session Id Here Inside "*"
-  OWNER_NUMBER: "263719647303", // Put Owner Number Here 
-  PREFIX: ".", // Default Prefix You Can Change To .,*!?+-&%#@
+  SESSION_ID: "SUBZERO-MD;;;eYk3EZDQ#BVAoAm1tdxdUQ5o3YV3jzhx2zk34stqR1H8JN7nQcoo", // Put Session Id Here Inside "*"
+  OWNER_NUMBER: "2349160224621", // Put Owner Number Here 
+  PREFIX: "💔", // Default Prefix You Can Change To .,*!?+-&%#@
   CDN: "https://mrfrankk-cdn.hf.space" // Dont change this part
 };
 
